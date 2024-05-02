fbaacc9eea7b02ebc0bbfbe73c6e1eb2411426bd
macovei-alexandru <maccelmare@gmail.com>
2024-05-02 15:53:56 +0300

    updated postbuild.bat to move models and shaders directories

--- Ancient-City/postbuild.bat
+++ Ancient-City/postbuild.bat
@@ -1,11 +1,15 @@
 @echo off
 
-set "source=..\_external\Assimp\bin\x64\"
-set "dest=..\x64\Debug\"
-set "files=assimp-vc143-mt.dll"
+copy "..\_external\Assimp\bin\x64\assimp-vc143-mt.dll" "..\x64\Debug\assimp-vc143-mt.dll"
 
-for %%f in (%files%) do (
-    if not exist "%dest%%%f" (
-        copy "%source%%%f" "%dest%%%f"
-    )
-)
+REM set "source=..\_external\Assimp\bin\x64\"
+REM set "dest=..\x64\Debug\"
+REM set "files=assimp-vc143-mt.dll"
+REM for %%f in (%files%) do (
+    REM if not exist "%dest%%%f" (
+        REM copy "%source%%%f" "%dest%%%f"
+    REM )
+REM )
+
+xcopy "Models" "..\x64\Debug\Models" /e /i /y
+xcopy "Shaders" "..\x64\Debug\Shaders" /e /i /y
